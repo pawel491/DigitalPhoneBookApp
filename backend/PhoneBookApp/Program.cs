c8e5ffc43f42236186d8a9234aa10e9ec1b71fc5
pawel491 <s197977@student.pg.edu.pl>
2026-03-25 14:04:53 +0100

    refactor: removed old unused code from Program.cs

--- backend/PhoneBookApp/Program.cs
+++ backend/PhoneBookApp/Program.cs
@@ -1,10 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
 using PhoneBookApp.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -19,47 +19,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.MapGet("/test-db", async (IConfiguration config) =>
-{
-    var connectionString = config.GetConnectionString("DefaultConnection");
-
-    try
-    {
-        await using var connection = new NpgsqlConnection(connectionString);
-        await connection.OpenAsync();
-
-        return Results.Ok("Successfully connected with postgresql db");
-    }
-    catch (Exception ex)
-    {
-        return Results.Problem($"Sth went wrong: {ex.Message}");
-    }
-});
-
 app.InitializeDatabase();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
