e7f22c7899953c33f3be81d1477e58ece230b3d9
pawel491 <s197977@student.pg.edu.pl>
2026-03-25 15:16:41 +0100

    feat: add endpoints for all operations on contacts, enchance contact mapper

--- backend/PhoneBookApp/Controllers/PhoneContactController.cs
+++ backend/PhoneBookApp/Controllers/PhoneContactController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PhoneBookApp.Data;
 using PhoneBookApp.Mappers;
+using PhoneBookApp.Model.Dto;
 
 namespace PhoneBookApp.Controllers;
 
@@ -23,5 +24,57 @@ public class PhoneContactController : ControllerBase
 
         var dtos = phoneContacts.Select(pc => pc.ToDto());
         return Ok(dtos);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetPhoneContact(int id)
+    {
+        var phoneContact = await _dbContext.PhoneContacts.FindAsync(id);
+        if (phoneContact == null)
+        {
+            return NotFound();
+        }
+        return Ok(phoneContact.ToDto());
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreatePhoneContact([FromBody] PhoneContactDto phoneContactDto)
+    {
+        var phoneContact = phoneContactDto.ToEntity();
+
+        _dbContext.PhoneContacts.Add(phoneContact);
+        await _dbContext.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetPhoneContact), new { id = phoneContact.Id }, phoneContact.ToDto());
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdatePhoneContact([FromBody] PhoneContactDto phoneContactDto, int id)
+    {
+        var phoneContact = await _dbContext.PhoneContacts.FindAsync(id);
+        if (phoneContact == null)
+        {
+            return NotFound();
+        }
+
+        phoneContact.UpdateEntity(phoneContactDto);
+        await _dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePhoneContact(int id)
+    {
+        var phoneContact = await _dbContext.PhoneContacts.FindAsync(id);
+        if (phoneContact == null)
+        {
+            return NotFound();
+        }
+
+        _dbContext.PhoneContacts.Remove(phoneContact);
+        await _dbContext.SaveChangesAsync();
+
+        return NoContent();
     }
 }
--- backend/PhoneBookApp/Mappers/PhoneContactMapper.cs
+++ backend/PhoneBookApp/Mappers/PhoneContactMapper.cs
@@ -6,11 +6,24 @@ namespace PhoneBookApp.Mappers;
 
 public static class PhoneContactMapper
 {
-    public static PhoneContactDto ToDto(this PhoneContact phoneContact)
+    public static PhoneContactDto ToDto(this PhoneContact entity)
     {
         return new PhoneContactDto(
-            phoneContact.Name,
-            phoneContact.PhoneNumber
+            entity.Name,
+            entity.PhoneNumber
         );
+    }
+    public static PhoneContact ToEntity(this PhoneContactDto phoneContactDto)
+    {
+        return new PhoneContact
+        {
+            Name = phoneContactDto.Name,
+            PhoneNumber = phoneContactDto.PhoneNumber
+        };
+    }
+    public static void UpdateEntity(this PhoneContact entity, PhoneContactDto phoneContactDto)
+    {
+        entity.Name = phoneContactDto.Name;
+        entity.PhoneNumber = phoneContactDto.PhoneNumber;
     }
 }
